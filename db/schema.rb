05ecccb242e910752d9f54cc7c7f4b1a6d9bab8c
haeol <k.tom_1@yahoo.com>
2017-04-10 14:36:08 -0700

    working on seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170407080237) do
+ActiveRecord::Schema.define(version: 20170410022432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.text    "description"
-    t.integer "user_id"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
   end
 
   create_table "posts", force: :cascade do |t|
-    t.integer "original_poster_id"
-    t.integer "rating"
-    t.string  "url"
-    t.integer "user_id"
-    t.text    "description"
+    t.integer  "original_poster_id"
+    t.float    "rating"
+    t.string   "url"
+    t.integer  "user_id"
+    t.text     "description"
+    t.string   "name"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.index ["original_poster_id"], name: "index_posts_on_original_poster_id", using: :btree
     t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
@@ -60,6 +65,7 @@ ActiveRecord::Schema.define(version: 20170407080237) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
+    t.string   "username"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+require 'pp' #TODO debugging
+
+# HOW TO RUN
+# rake db:seed ENV_NAME=yes
+# example: rake db:seed movie=yes will run the EVN["movie"] portion
+
+
+if ENV["movie"]
+  user = User.all[0]
+
+  csv_text = File.read(Rails.root.join('lib', 'seeds', 'movie_metadata.csv'))
+  csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+  csv.each do |row|
+
+    params = {
+      rating: row["imdb_score"],
+      url:    row["movie_imdb_link"],
+      name:   row["movie_title"],
+      description: "I am a bot. Beep boop."
+    }
+    p = user.posts.build(params)
+    p.save
+  end
+end
+
+
+
+#csv_text = File.read(Rails.root.join('lib', 'seeds', 'movie_metadata.csv'))
+#csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+#csv.each do |row|
+#
+#  params = {
+#    rating: row["imdb_score"],
+#    url:    row["movie_imdb_link"],
+#    name:   row["movie_title"],
+#    description: "I am a bot. Beep boop."
+#  }
+#  p = user.posts.build(params)
+#  p.save
+#end
