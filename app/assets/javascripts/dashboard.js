2a1aebe0128a801cbf0852d9958aa026ac4837a2
haeol <k.tom_1@yahoo.com>
2017-04-13 23:41:03 -0700

    added dashboard js file for clicking tabs, removed old logo coloring

---
+++ app/assets/javascripts/dashboard.js
@@ -0,0 +1,13 @@
+
+/*
+$(document).ready(function() {
+    $('.nav-item').click(function() {
+      $(this).addClass('active').siblings('.active').removeClass('active');
+    });
+});
+*/
+$(document).ready(function() {
+    $('.nav-item').click(function() {
+      $(this).addClass('active').siblings('.active').removeClass('active');
+    });
+});
