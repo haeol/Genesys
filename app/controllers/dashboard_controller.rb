0fa1ea51c5444edc9754cabe7b1f38836d932fe6
haeol <k.tom_1@yahoo.com>
2017-04-13 23:45:21 -0700

    updated index to include js for tabs

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,20 @@
 class DashboardController < ApplicationController
   def index
     #TODO offset and infinite scrolling instead of limit 50
-    @posts = Post.filter(params.slice(:name, :tag, :tag_id, :starts_with)).limit(50)
+    posts = Post.filter(params.slice(:name, :tag, :tag_id, :starts_with)).limit(50)
+    render locals: {
+      posts: posts,
+      tags: Tag.all.limit(20)# TODO trending or defaults
+    }
+    
+    respond_to do |format|
+      format.html # calls index.html.erb
+      format.js # calls index.js.erb
+    end
   end
+
+  def is_active?(link)
+    current_page?(link) ? "active" : ""
+  end
+  helper_method :is_active?
 end
