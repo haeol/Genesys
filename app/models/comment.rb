5cfda229f108209bb1a69a0720cce5345c895d73
haeol <k.tom_1@yahoo.com>
2017-05-03 21:22:25 -0700

    reformatted post show to show description in a div, adjusted contents a little

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,8 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   include Filterable # located in ./concerns/filterable.rb
+  require 'action_view'
+  include ActionView::Helpers::DateHelper
 
 	# filter can ONLY take these parameters
   scope :starts_with, -> (word) {
@@ -12,4 +14,9 @@ class Comment < ApplicationRecord
   def username
     user.username
   end
+
+  def submit_time_diff
+    "#{distance_of_time_in_words(self.created_at, Time.now)}"
+  end
+
 end
